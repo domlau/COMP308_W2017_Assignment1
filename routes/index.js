93548a19046dd228ccc8cf56160d8be0362edb81
domlau <dominic.k.y.lau@gmail.com>
2017-02-04 21:36:00 -0500

    added image, create content, partials

--- routes/index.js
+++ routes/index.js
@@ -5,20 +5,37 @@ currentDate = currentDate.toLocaleString();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', {
+  res.render('content/index', {
     
     title: 'Dominic Portfolio Site',
-    date: currentDate
   });
 });
 
 /* GET about page */
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'About Me' });
+  res.render('content/about', { 
+    title: 'About Me' 
+  });
+});
+
+/* GET projects page */
+router.get('/projects', (req, res, next) => {
+  res.render('content/projects', { 
+    title: 'My Projects' 
+  });
+});
+
+/* GET services page */
+router.get('/services', (req, res, next) => {
+  res.render('content/services', { 
+    title: 'My Services' 
+  });
 });
 
 /* GET contact page */
 router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'Contact' });
+  res.render('content/contact', { 
+    title: 'Contact Me' 
+  });
 });
 module.exports = router;
